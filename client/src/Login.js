75fdd86949e1378523977ba1d960053499daffa7
hasham56 <hashammeher@gmail.com>
2022-07-20 14:27:54 +0500

    added bazmi app

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Container } from "react-bootstrap"
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=8523f61c4a89414aae57d47248327ed2&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
   return (
--- client/src/TrackSearchResult.js
+++ client/src/TrackSearchResult.js
@@ -11,7 +11,7 @@ export default function TrackSearchResult({ track, chooseTrack }) {
       style={{ cursor: "pointer" }}
       onClick={handlePlay}
     >
-      <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
+      <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} alt='song_img' />
       <div className="ml-3">
         <div>{track.title}</div>
         <div className="text-muted">{track.artist}</div>
